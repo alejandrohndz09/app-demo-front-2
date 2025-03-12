d6b73bac95a9d55ba7dcbc5405e66d21c70b53d0
Flavio Alejandro Molina Hernandez <flavio.molina@mh.gob.sv>
2025-03-12 15:36:05 -0600

    front en proceso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+import {MenuComponent} from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ButtonModule, CardModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'appdemo2';
