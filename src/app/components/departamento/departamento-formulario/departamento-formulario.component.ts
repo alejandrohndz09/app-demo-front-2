def201f089fa431238e799d7f3623e81c7d9f5ac
Flavio Alejandro Molina Hernandez <flavio.molina@mh.gob.sv>
2025-03-14 15:36:38 -0600

    Front avance

--- src/app/components/departamento/departamento-formulario/departamento-formulario.component.ts
+++ src/app/components/departamento/departamento-formulario/departamento-formulario.component.ts
@@ -7,6 +7,8 @@ import {InputText} from 'primeng/inputtext';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {Departamento} from '../../../models/departamento.model';
 import {DepartamentoDataService} from '../../../services/departamento-data.service';
+import {NgClass, NgIf} from '@angular/common';
+import {MessageService} from 'primeng/api';
 
 @Component({
   selector: 'departamento-formulario',
@@ -17,7 +19,9 @@ import {DepartamentoDataService} from '../../../services/departamento-data.servi
     InputMask,
     InputText,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    NgClass,
+    NgIf
   ],
   templateUrl: './departamento-formulario.component.html',
   styles: ``
@@ -27,25 +31,67 @@ export class DepartamentoFormularioComponent {
   @Input() visible!: boolean;
   @Output() onHide = new EventEmitter<boolean>();
   @Output() onSave = new EventEmitter<Departamento>();
-  errors=[]
+  errors: { field: string, message: string } [] = [];
 
-  constructor(private departamentoService: DepartamentoDataService) {
-
+  constructor(private departamentoService: DepartamentoDataService, private messageService: MessageService) {
   }
 
   convertToUpperCase() {
-    //this.departamento.codigo=this.departamento.codigo?.toUpperCase()
+    this.departamento.codigo = this.departamento.codigo?.toUpperCase()
   }
 
   guardar(departamento: Departamento) {
-console.log(this.departamento);
-    this.departamentoService.create("",departamento).subscribe(
-      data => {
-        this.onSave.emit(data);
-      },
-      error => {
-        console.log(error);
-      }
-    )
+    if (!departamento.id) {
+      this.departamentoService.create("", departamento).subscribe(
+        data => {
+          this.visible = false;
+          this.errors = [];
+          this.onSave.emit(data);
+        },
+        error => {
+          if (error.error && error.error.violations) {
+            this.errors = error.error.violations.map((v: any) => ({
+              field: v.field.split('.').pop(),
+              message: v.message
+            }));
+          }
+        }
+      )
+    }else{
+      this.departamentoService.update("", departamento.id,departamento).subscribe(
+        data => {
+          this.visible = false;
+          this.errors = [];
+          this.onSave.emit(data);
+        },
+        error => {
+          if (error.error && error.error.violations) {
+            this.errors = error.error.violations.map((v: any) => ({
+              field: v.field.split('.').pop(),
+              message: v.message
+            }));
+          }else{
+            this.messageService.add({severity: 'error', summary:error.message, detail: error});
+          }
+        }
+      )
+    }
+
+  }
+
+  cerrar() {
+    this.visible = false;
+    this.errors = [];
+    this.onHide.emit(this.visible);
+  }
+
+  // Función auxiliar para obtener el mensaje de error de un campo
+  getError(field: string): string | null {
+    return this.errors.find(e => e.field === field)?.message || null;
+  }
+
+// Función para verificar si un campo tiene error
+  hasError(field: string): boolean {
+    return this.errors.some(e => e.field === field);
   }
 }
--- src/app/components/departamento/departamento.component.ts
+++ src/app/components/departamento/departamento.component.ts
@@ -5,7 +5,7 @@ import {TableModule} from 'primeng/table';
 import {MiMenuComponent} from '../generics/menu/toggle-menu.component';
 import {Departamento} from '../../models/departamento.model';
 import {Toast} from 'primeng/toast';
-import {MessageService} from 'primeng/api';
+import {ConfirmationService, MessageService} from 'primeng/api';
 import {DepartamentoDataService} from '../../services/departamento-data.service';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {Tooltip} from 'primeng/tooltip';
@@ -15,6 +15,7 @@ import {FormsModule} from '@angular/forms';
 import {FloatLabel} from 'primeng/floatlabel';
 import {InputMask} from 'primeng/inputmask';
 import {DepartamentoFormularioComponent} from './departamento-formulario/departamento-formulario.component';
+import {ConfirmDialog} from 'primeng/confirmdialog';
 
 @Component({
   selector: 'app-departamento',
@@ -26,41 +27,83 @@ import {DepartamentoFormularioComponent} from './departamento-formulario/departa
     Toast,
     HttpClientModule,
     Tooltip,
-    Dialog,
-    InputText,
     FormsModule,
-    FloatLabel,
-    InputMask,
-    DepartamentoFormularioComponent
+    DepartamentoFormularioComponent,
+    ConfirmDialog
   ],
-  providers: [MessageService, DepartamentoDataService],
+  providers: [MessageService, DepartamentoDataService, ConfirmationService],
   templateUrl: './departamento.component.html',
   styles: ``
 })
 export class DepartamentoComponent {
   departamentos!: Departamento[];
-  departamento!: Departamento;
-  visible: boolean=false;
+  departamento: Departamento;
+  visible: boolean = false;
 
 
-  constructor(private dataServices: DepartamentoDataService, private messageService: MessageService) {
+  constructor(private dataServices: DepartamentoDataService, private confirmationService: ConfirmationService, private messageService: MessageService) {
     this.dataServices.getAll("").subscribe(
       departamentos => (this.departamentos = departamentos),
       error => this.messageService.add(error)
     );
-    this.departamento=new Departamento();
+    this.departamento = new Departamento();
   }
 
   actualizarLista(departamento: Departamento) {
 
-      const index = this.departamentos.findIndex(d => d.id === departamento.id);
-      if (index !== -1) {
-        this.departamentos[index] = departamento;
-      }else{
-        this.departamentos.push(departamento);
-      }
-    this.messageService.add({ severity: 'info', detail: 'Operación exitosa' })
-    this.departamento=new Departamento();
+    const index = this.departamentos.findIndex(d => d.id === departamento.id);
+    if (index !== -1) {
+      this.departamentos[index] = departamento;
+    } else {
+      this.departamentos.push(departamento);
+    }
+    this.messageService.add({
+      severity: 'success', summary: "Operación exitosa", detail: 'Se han guardado los' +
+        ' cambios.'
+    })
+    this.departamento = new Departamento();
+  }
 
+  editar(dep: any) {
+    this.departamento = dep;
+    this.visible = true;
+  }
+
+  eliminar(event: Event, id: number) {
+
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: '¿Está seguro que desea eliminar el registro?',
+      header: 'Confirmación',
+      closable: true,
+      closeOnEscape: true,
+      icon: 'pi pi-exclamation-triangle',
+      rejectButtonProps: {
+        label: 'Cancelar',
+        severity: 'secondary',
+      },
+      acceptButtonProps: {
+        label: 'Confirmar',
+      },
+      accept: () => {
+        this.dataServices.delete("", id).subscribe(
+          result => {
+            this.messageService.add({
+              severity: 'success', summary: 'Operación exitosa', detail: 'Se ha eliminado el' +
+                ' registro.'
+            });
+            this.departamentos = this.departamentos.filter(d => d.id !== id);
+          },
+          error => {
+            if (error.error && error.message) {
+              console.log(error);
+              this.messageService.add({severity: 'error', summary: 'Ocurrió un error', detail: error.error.message});
+            } else {
+              this.messageService.add({severity: 'error', summary: 'Ocurrió un error', detail: 'Error desconocido.'});
+            }
+          }
+        );
+      }
+    });
   }
 }
